f650c8346e90b06a172dcaa97a2e3b4769a67e95
adhammhaydat <adhammohidat123@gmail.com>
2021-10-13 21:40:48 +0300

    test:test12 done

--- pythonic_garage_band/pythonic_garage_band.py
+++ pythonic_garage_band/pythonic_garage_band.py
@@ -17,12 +17,15 @@ class Guitarist(Musician):
     
     def __init__(self,name):
         self.name=name
+        
     def __str__(self):
         return f"My name is {self.name} and I play guitar"
     def __repr__(self):
         return f"Guitarist instance. Name = {self.name}"    
     def get_instrument(self):
         return "guitar"
+    def play_solo(self):
+        return "face melting guitar solo"    
 
 class Drummer(Musician):
     def __init__(self,name):
@@ -35,6 +38,8 @@ class Drummer(Musician):
         return f"Drummer instance. Name = {self.name}"    
     def get_instrument(self): 
         return "drums"
+    def play_solo(self):
+        return "rattle boom crash"     
 class Bassist(Musician):
     def __init__(self,name):
         self.name=name
@@ -46,3 +51,7 @@ class Bassist(Musician):
         return f"Bassist instance. Name = {self.name}"  
     def get_instrument(self): 
         return "bass"
+
+    def play_solo(self):
+        return "bom bom buh bom"  
+        
