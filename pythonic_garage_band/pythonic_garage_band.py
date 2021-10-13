416d7b3468115c5cab24a9e753ccba3f7c161b1a
adhammhaydat <adhammohidat123@gmail.com>
2021-10-13 20:54:46 +0300

    test:test1 done

---
+++ pythonic_garage_band/pythonic_garage_band.py
@@ -0,0 +1,26 @@
+class Musician():
+    def __init__(self,members,name):
+        self.members=members
+        
+
+class Band(Musician):
+    def __init__(self,name):
+
+        self.name=name
+
+    def play_solos():
+        pass    
+
+class Guitarist(Band):
+    
+    def __init__(self,name):
+        self.name=name
+    def __str__(self):
+        return f"My name is {self.name} and I play guitar"
+    def __repr__(self):
+        return f"Guitarist instance. Name = {self.name}"    
+
+class Bassist:
+    pass
+class Drummer:
+    pass
