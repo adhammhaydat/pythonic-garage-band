421b42259739ca3e5538ec25cf8653159a6e936d
adhammhaydat <adhammohidat123@gmail.com>
2021-10-13 23:10:40 +0300

    add url

---
+++ README.md
@@ -0,0 +1,1 @@
+pull req:https://github.com/adhammhaydat/pythonic-garage-band/pull/1
--- pythonic_garage_band/pythonic_garage_band.py
+++ pythonic_garage_band/pythonic_garage_band.py
@@ -1,17 +1,21 @@
 class Musician():
-    def __init__(self,members,name):
+    to_list=[]
+    def __init__(self,members):
         self.members=members
-        
 
 class Band(Musician):
     def __init__(self,name,members):
 
         self.name=name
         self.members=members
-    def play_solos():
-        pass  
-    # def get_instrument(self): 
-    #     return self.members  
+
+    def play_solos(self):
+         return self.name     
+
+    
+
+    def to_list(self): 
+         return self.members  
 
 class Guitarist(Musician):
     
@@ -25,21 +29,8 @@ class Guitarist(Musician):
     def get_instrument(self):
         return "guitar"
     def play_solo(self):
-        return "face melting guitar solo"    
-
-class Drummer(Musician):
-    def __init__(self,name):
-        self.name=name
-
-    def __str__(self):
-        return f"My name is {self.name} and I play drums"
-
-    def __repr__(self):
-        return f"Drummer instance. Name = {self.name}"    
-    def get_instrument(self): 
-        return "drums"
-    def play_solo(self):
-        return "rattle boom crash"     
+        return "face melting guitar solo"  
+          
 class Bassist(Musician):
     def __init__(self,name):
         self.name=name
@@ -54,4 +45,18 @@ class Bassist(Musician):
 
     def play_solo(self):
         return "bom bom buh bom"  
-        
+class Drummer(Musician):
+    def __init__(self,name):
+        self.name=name
+
+    def __str__(self):
+        return f"My name is {self.name} and I play drums"
+
+    def __repr__(self):
+        return f"Drummer instance. Name = {self.name}"    
+    def get_instrument(self): 
+        return "drums"
+    def play_solo(self):
+        return "rattle boom crash"     
+
+      
